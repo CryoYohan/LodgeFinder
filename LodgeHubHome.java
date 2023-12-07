6f6ff4dbbe6a93686b08b72b7701f8d9e476c7da
Cyril John T. Ypil <cyrilypil@gmail.com>
2023-12-07 20:04:56 +0800

    Finished Right Panel BG by Clark

--- LodgeHubHome.java
+++ LodgeHubHome.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.ImageIcon;
@@ -15,9 +16,8 @@ public class LodgeHubHome extends JFrame {
     final int FRAME_SIZE_WIDTH = 1440;
     final int FRAME_SIZE_HEIGHT = 1025;
 
-    //NavBar at the Left Side
-    JPanel navBar = new JPanel(); 
-    JPanel homeBar = new JPanel();
+    // NavBar at the Left Side
+    JPanel navBar = new JPanel();
     JButton Profile = new CircleJButton("Cyril John");
     JButton Home = new HomeJButton("Home");
     JButton Favorites = new CircleJButton("Favorites");
@@ -27,12 +27,7 @@ public class LodgeHubHome extends JFrame {
     ImageIcon heart = new ImageIcon("Image/heart.png");
     ImageIcon logout = new ImageIcon("Image/Logout.png");
 
-    //Title and Quote
-    JPanel container = new JPanel();
-    JLabel title = new JLabel();
-    ImageIcon lodgeTitle = new ImageIcon("Image/Logo.png");
-
-    //HOME INTERFACE
+    // HOME INTERFACE
     JPanel homeContainer = new JPanel();
     JLabel homeBackground = new JLabel();
     ImageIcon homeWallpaper = new ImageIcon("Image/HomeWallpaper.png");
@@ -40,26 +35,30 @@ public class LodgeHubHome extends JFrame {
 
     LodgeHubHome() {
         this.setSize(FRAME_SIZE_WIDTH, FRAME_SIZE_HEIGHT);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);
-		this.setTitle("LodgeFinder");
-		this.setVisible(true);	
-		this.setLayout(new BorderLayout());
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setTitle("LodgeFinder");
+        this.setVisible(true);
+        this.setLayout(new BorderLayout());
         this.add(NavBar(), BorderLayout.WEST);
-        this.add(TitleBar());
         this.add(Home());
-		this.setLocationRelativeTo(null);;
+        this.setLocationRelativeTo(null);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-				int confirm = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit Program", JOptionPane.YES_OPTION);
-				if(confirm == JOptionPane.YES_OPTION) {
-					setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				} else {
-					setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-				}
+                int confirm = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit Program", JOptionPane.YES_OPTION);
+                if (confirm == JOptionPane.YES_OPTION) {
+                    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                } else {
+                    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                }
             }
         });
+    }
+
+    private ImageIcon resizeImageIcon(ImageIcon icon, int width, int height) {
+        Image image = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(image);
     }
 
     public JPanel NavBar() {
@@ -67,19 +66,19 @@ public class LodgeHubHome extends JFrame {
         navBar.setPreferredSize(new Dimension(245, 1025));
         navBar.setBackground(new Color(100, 178, 255));
 
-        //Buttons in navigation
+        // Buttons in navigation
         navBar.add(Profile);
         navBar.add(Home);
         navBar.add(Favorites);
         navBar.add(Logout);
-        //Profile Bar
+        // Profile Bar
         Profile.setIcon(profile);
         Profile.setBounds(56, 26, 140, 170);
         Profile.setFont(new Font("Segoe UI", Font.BOLD, 18));
         Profile.setHorizontalTextPosition(JButton.CENTER);
         Profile.setVerticalTextPosition(JButton.BOTTOM);
         Profile.setIconTextGap(15);
-        //Home Bar
+        // Home Bar
         Home.setIcon(home);
         Home.setBounds(0, 206, 246, 170);
         Home.setFont(new Font("Segoe UI", Font.BOLD, 18));
@@ -87,14 +86,14 @@ public class LodgeHubHome extends JFrame {
         Home.setHorizontalTextPosition(JButton.CENTER);
         Home.setVerticalTextPosition(JButton.BOTTOM);
         Home.setIconTextGap(15);
-        //Favorite Bar
+        // Favorite Bar
         Favorites.setIcon(heart);
         Favorites.setBounds(65, 387, 115, 170);
         Favorites.setFont(new Font("Segoe UI", Font.BOLD, 18));
         Favorites.setHorizontalTextPosition(JButton.CENTER);
         Favorites.setVerticalTextPosition(JButton.BOTTOM);
         Favorites.setIconTextGap(15);
-        //Logout button
+        // Logout button
         Logout.setIcon(logout);
         Logout.setBounds(55, 920, 130, 30);
         Logout.setFont(new Font("Segoe UI", Font.BOLD, 16));
@@ -103,35 +102,18 @@ public class LodgeHubHome extends JFrame {
 
         return navBar;
     }
-    
-    public JPanel TitleBar() {
-        container.setLayout(null);
-        container.setBounds(245, 10, 1195, 140);
-        container.setBackground(new Color(255, 255, 255));
-        container.add(title);
-
-        //Title
-        title.setIcon(lodgeTitle);
-        title.setBounds(32, 8, 1195, 133);
-        title.setText("\"WHERE COMFORTS MEETS CONVENIENCE\"");
-        title.setFont(new Font("Segoe UI", Font.PLAIN, 25));
-        title.setForeground(new Color(0, 32, 50));
-        title.setIconTextGap(30);
-
-        return container;
-    }
 
     public JPanel Home() {
         homeContainer.setLayout(null);
         homeContainer.setBounds(140, 245, 1195, 900);
         homeContainer.add(homeBackground);
         homeContainer.add(locationBox);
-        
-        //JLabel for Home Wallpaper
-        homeBackground.setIcon(homeWallpaper);
-        homeBackground.setBounds(0, 170, 1180, 818);
+
+        // JLabel for Home Wallpaper
+        homeBackground.setIcon(resizeImageIcon(homeWallpaper, 1180, 840));
+        homeBackground.setBounds(0, 0, 1300, 840);
         homeBackground.add(locationBox);
 
         return homeContainer;
     }
-}
+    }
